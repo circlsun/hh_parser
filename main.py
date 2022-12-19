98603a2654a1cb781a963625ba02692048f6db28
circlsun <circlsun@gmail.com>
2022-12-19 17:06:50 +0300

    Add count vacancies

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import requests
 from pprint import pprint
 
 
-def fetch_hh_vacancies():
+def fetch_hh_vacancies(period=None):
     base_url = 'https://api.hh.ru/vacancies'
     payload = {
         'text': 'программист',
-        'area': 1
+        'area': 1,
+        'period': period
     }
     response = requests.get(base_url, params=payload)
     response.raise_for_status()
-    pprint(response.json())
+    return response.json()
 
 
 def main():
-    fetch_hh_vacancies()
+    print(fetch_hh_vacancies()['found'] - fetch_hh_vacancies(period=30)['found'])
 
 
 if __name__ == '__main__':
