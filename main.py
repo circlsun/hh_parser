dd3012e768eaf9a5c71cb887efdd5d9e5d03a842
Matveev Andrey <circlsun@gmail.com>
2022-12-19 23:25:36 +0300

    Add rating vacancies

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
 import requests
+import json
 from pprint import pprint
+
+lenguages = (
+    'Javascript',
+    'Python',
+    'Go',
+    'Java',
+    'Kotlin',
+    'PHP',
+    'C#',
+    'Swift',
+    'R',
+    'Ruby',
+    'C/C++',
+    'Matlab',
+    'TypeScript',
+    'Scala'
+)
 
 
 def fetch_hh_vacancies(period=None):
+    rating = {}
+    base_url = 'https://api.hh.ru/vacancies'
+    for language in lenguages:
+
+        payload = {
+            'text': f'Программист {language}',
+            'area': 1,
+            'period': period
+        }
+        response = requests.get(base_url, params=payload)
+        response.raise_for_status()
+        count = response.json()['found']
+        if count >= 100:
+            rating[language] = count
+    return dict(sorted(rating.items(), key=lambda x: x[1], reverse=True))
+
+
+def fetch_salary():
+    salaries = {}
     base_url = 'https://api.hh.ru/vacancies'
     payload = {
-        'text': 'программист',
-        'area': 1,
-        'period': period
+        'text': 'Программист Python',
+        'area': 1
     }
     response = requests.get(base_url, params=payload)
     response.raise_for_status()
@@ -15,7 +51,7 @@ def fetch_hh_vacancies(period=None):
 
 
 def main():
-    print(fetch_hh_vacancies()['found'] - fetch_hh_vacancies(period=30)['found'])
+    print(json.dumps(fetch_hh_vacancies(period=30), indent=4))
 
 
 if __name__ == '__main__':
